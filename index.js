eeb63d804d45a7f5a0827fb901837167ee2ef65e
Mubashir Kabir <97.m.kabir@gmail.com>
2022-11-08 00:23:51 +0600

    db connection, .env and get all services api end poin created

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 //required packages
 const express = require("express");
 const cors = require("cors");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
 
 const app = express();
 
@@ -13,6 +15,38 @@ const port = process.env.PORT || 5000;
 //root route
 app.get("/", (req, res) => {
   res.send("Server is Up...");
+});
+
+//mongodb connection
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.erzixne.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri);
+const dbServices = client.db("koni's-kitchen").collection("services");
+const dbConnection = async () => {
+  try {
+    await client.connect();
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.log("Database connection error", error.name, error.message);
+  }
+};
+dbConnection();
+
+//get all services
+app.get("/services", async (req, res) => {
+  try {
+    const cursor = await dbServices.find({});
+    const services = await cursor.toArray();
+    res.send({
+      status: true,
+      data: services,
+    });
+  } catch (err) {
+    console.log(err.name, err.message);
+    res.send({
+      status: "error",
+      data: err.name,
+    });
+  }
 });
 
 //server listener
